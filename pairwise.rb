0d6ee4bcbd6a453f07dc529482734f7acdfbe5b0
Joseph Wilk <joe@josephwilk.net>
2009-12-10 23:00:28 +0000

    Split out specs and finish a limited ipo_v

--- pairwise.rb
+++ pairwise.rb
@@ -1,7 +1,6 @@
 require 'rubygems'
-require 'ruby-debug'
 require 'set'
-require 'spec'
+require 'ruby-debug'
 
 class Pairwise
 
@@ -11,36 +10,30 @@ class Pairwise
 
     def generate(inputs)
       raise InvalidInput, "Minimum of 2 inputs are required to generate pairwise test set" if inputs.length < 2 || inputs[0].values[0].empty? && inputs[1].values[0].empty?
-      test_set = generate_pairs_between(inputs[0].values[0], [inputs[1]])
+
+      inputs_without_labels = inputs.map {|input| input.values[0]}
+     
+      test_set = generate_pairs_between(inputs_without_labels[0], [inputs_without_labels[1]])
       count = 0
-      if inputs.size > 2
-        for i in 2.. inputs.size-1
-          test_set, pi = ipo_h(test_set, inputs[i].values[0], inputs[0..(i-1)])
-          #ipo_v(test_set, pi)
+      if inputs_without_labels.size > 2
+        for i in 2.. inputs_without_labels.size-1
+          test_set, pi = ipo_h(test_set, inputs_without_labels[i], inputs_without_labels[0..(i-1)])
+          test_set = ipo_v(test_set, pi)
         end
       end
       test_set
     end
 
-    def pairs_covered(value, pairs)
-      covered_count = 0
-      pairs.each do |pair|
-        covered = value.reduce(true){|covered, single_value| covered && pair.include?(single_value)}
-        covered_count += 1 if covered
-      end
-      covered_count
-    end
-    
     private
 
+    #TODO: Look at using zip when extending tests
     def ipo_h(test_set, parameter_i, inputs)
       pi = generate_pairs_between(parameter_i, inputs)
       q = parameter_i.size
-      
+
       if test_set.size <= q
         for j in 0..test_set.size do
           extended_test = test_set[j] << parameter_i[j]
-
           pi = remove_pairs_covered_by(extended_test, pi)
         end
       else
@@ -50,21 +43,45 @@ class Pairwise
         end
 
         for i in q...test_set.size do
-          value = select_value_that_covers_most_pairs(test_set[i], parameter_i, pi)
-          extended_test = test_set[i] << value
+          extended_test = select_value_that_covers_most_pairs(test_set[i], parameter_i, pi)
           pi = remove_pairs_covered_by(extended_test, pi)
+          test_set[i] = extended_test
         end
       end
 
       [test_set, pi]
     end
 
+    def ipo_v(test_set, pi)
+      new_test_set = []
+
+      pi.each do |(p1,p2)|
+        if wild_card_index = contains_wild_card?(new_test_set)
+          new_test_set[wild_card_index] = replace_wild_card(new_test_set[wild_card_index], p2)
+        else
+          #TODO: need to handle arbitrary set length
+          new_test_set << [p2, :wild_card, p1]
+        end
+      end
+
+      test_set + new_test_set
+    end
+
+    def contains_wild_card?(test_set)
+      wild_card_list = test_set.map{|set| set.include?(:wild_card) ? true : false}
+      wild_card_list.rindex(true)
+    end
+
+    def replace_wild_card(set, replace_with_value)
+      set.map{|value| value == :wild_card ? replace_with_value : value}
+    end
+    
     def generate_pairs_between(parameter_i, inputs)
       pairs = []
       parameter_i.each do |p|
-        inputs.each do |q|
-          q.values[0].each do |q1|
-            pairs << [p, q1]
+        inputs.each do |input|
+          input.each do |q|
+            pairs << [p, q]
           end
         end
       end
@@ -72,46 +89,38 @@ class Pairwise
     end
 
     def remove_pairs_covered_by(extended_test, pi)
-      pi
+      pi.select{ |pair| !matches_pair(pair, extended_test)}
     end
 
     def select_value_that_covers_most_pairs(test_value, parameter_i, pi)
-      parameter_i.each do |value|
-        temp_value = test_value << value
-        pairs_covered(temp_value, pi)
+      max_value = parameter_i[0]
+
+      selected_value = nil
+      parameter_i.reduce(0) do |most_covered, value|
+        temp_value = test_value.dup + [value]
+        covered = pairs_covered(temp_value, pi)
+        if covered > most_covered
+          selected_value = temp_value
+          covered
+        else
+          most_covered
+        end
       end
+      selected_value
     end
-    
+
+    def pairs_covered(value, pairs)
+      covered_count = 0
+      covered = pairs.reduce(0) do |covered, pair|
+        covered_count += 1 if matches_pair(pair, value)
+      end
+      covered_count
+    end
+
+    #TODO: use ruby and sets
+    def matches_pair(pair_1, pair_2)
+      pair_1.reduce(true){|matches, pair_value| matches && pair_2.include?(pair_value)}
+    end
+
   end
-end
-
-describe "pairwise" do
-
-  it "should be invalid when running with no input" do
-    lambda{ Pairwise.generate([]) }.should raise_error(Pairwise::InvalidInput)
-    lambda{ Pairwise.generate([{:A => []}]) }.should raise_error(Pairwise::InvalidInput)
-  end
-
-  it "should be invalid when running with only 1 input" do
-    lambda{ Pairwise.generate([{:A => [:A1, :A2]}])}.should raise_error(Pairwise::InvalidInput)
-  end
-  
-  it "should generate all pairs for two parameters" do
-    data = [{:A => [:A1, :A2]},
-            {:B => [:B1, :B2]}]
-
-    Pairwise.generate(data).should == [[:A1, :B1], [:A1, :B2], [:A2, :B1], [:A2, :B2]]
-  end
-
-  it "should generate pairs for three paramters" do
-    data = [{:A => [:A1, :A2]},
-            {:B => [:B1, :B2]},
-            {:C => [:C1 , :C2 , :C3 ]}]
-
-    Pairwise.generate(data).should == [[:A1, :B1, :C1],
-                                       [:A1, :B1, :C2],
-                                       [:A2, :B1, :C3],
-                                       [:A2, :B2, :C1]]
-  end
-
 end
---
+++ pairwise_spec.rb
@@ -0,0 +1,64 @@
+require 'rubygems'
+require 'spec'
+
+require File.dirname(__FILE__) + '/pairwise'
+
+describe "pairwise" do
+
+  it "should be invalid when running with no input" do
+    lambda{ Pairwise.generate([]) }.should raise_error(Pairwise::InvalidInput)
+    lambda{ Pairwise.generate([{:A => []}]) }.should raise_error(Pairwise::InvalidInput)
+  end
+
+  it "should be invalid when running with only 1 input" do
+    lambda{ Pairwise.generate([{:A => [:A1, :A2]}])}.should raise_error(Pairwise::InvalidInput)
+  end
+
+  it "should generate all pairs for two parameters" do
+    data = [{:A => [:A1, :A2]},
+            {:B => [:B1, :B2]}]
+
+    Pairwise.generate(data).should == [[:A1, :B1], [:A1, :B2], [:A2, :B1], [:A2, :B2]]
+  end
+
+
+  describe 'ipo horizontal growth' do
+    before(:each) do
+      @test_pairs = [[:A1, :B1], [:A1, :B2], [:A2, :B1], [:A2, :B2]]
+
+      @data = [[:A1, :A2],[:B1, :B2],[:C1 , :C2 , :C3 ]]
+    end
+    
+    it "should return pairs extended with C's inputs" do
+      test_set, _ = Pairwise.send(:ipo_h, @test_pairs, @data[2], @data[0..1])
+
+      test_set.should == [[:A1, :B1, :C1],
+                          [:A1, :B2, :C2],
+                          [:A2, :B1, :C3],
+                          [:A2, :B2, :C1]]
+    end
+
+    it "should return all the uncovered pairs" do
+      _, pi = Pairwise.send(:ipo_h, @test_pairs, @data[2], @data[0..1])
+
+      # We are getting the uncovered pairs in reverse
+      #pi.should == [[:A2, :C2],[:A1, :C3],[:B1, :C2],[:B2, :C3]]
+      # Cheat and check we get the list in reverse
+      pi.should == [[:C2, :A2], [:C2, :B1], [:C3, :A1], [:C3, :B2]]
+    end
+  end
+
+  it "should generate pairs for three paramters" do
+    data = [{:A => [:A1, :A2]},
+            {:B => [:B1, :B2]},
+            {:C => [:C1 , :C2 , :C3 ]}]
+
+    Pairwise.generate(data).should == [[:A1, :B1, :C1],
+                                       [:A1, :B2, :C2],
+                                       [:A2, :B1, :C3],
+                                       [:A2, :B2, :C1],
+                                       [:A2, :B1, :C2],
+                                       [:A1, :B2, :C3]]
+  end
+
+end
