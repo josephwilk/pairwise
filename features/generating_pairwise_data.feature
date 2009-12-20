7854fa57099094250046b6ddef1234a08238f603
Joseph Wilk <joe@josephwilk.net>
2009-12-20 21:55:37 +0000

    handle unordered yaml inputs

--- features/generating_pairwise_data.feature
+++ features/generating_pairwise_data.feature
@@ -3,7 +3,7 @@ Feature: Generating pairwise data
   As a tester
   I want a set of tests which is smaller than all the possible combinations of my specified inputs
 
-Scenario:
+Scenario: Ordered yaml inputs
   Given I have the yaml file "inputs.yml" containing:
     """
     - event with image: [Football, Basketball, Soccer]
@@ -23,5 +23,28 @@ Scenario:
     |Soccer|Football|Video|
     |Soccer|Basketball|Music|
     |Soccer|Soccer|Image|
+
+    """
+
+Scenario: Unorderd yaml inputs
+  Given I have the yaml file "inputs.yml" containing:
+    """
+    event with image: [Football, Basketball, Soccer]
+    event without image: [Football, Basketball, Soccer]
+    media: [Image, Video, Music]
+    """
+  When I run pairwise inputs.yml
+  Then I should see the output
+    """
+    |media|event without image|event with image|
+    |Image|Football|Football|
+    |Image|Basketball|Basketball|
+    |Image|Soccer|Soccer|
+    |Video|Football|Soccer|
+    |Video|Basketball|Football|
+    |Video|Soccer|Basketball|
+    |Music|Football|Basketball|
+    |Music|Basketball|Soccer|
+    |Music|Soccer|Football|
 
     """
--- lib/pairwise/cli.rb
+++ lib/pairwise/cli.rb
@@ -4,30 +4,42 @@ module Pairwise
   class Cli
     class << self
       def execute(args)
-        input_file = ARGV[0]
-        inputs = YAML.load_file(input_file)
-
-        test_set = Pairwise.test_set(inputs)
-        display(test_set, input_names(inputs))
+        new(args).execute
       end
+    end
 
-      private
-      def input_names(inputs)
-        inputs.map{|input| input.keys}.flatten
-      end
+    def initialize(args)
+      @input_file = args[0]
+    end
+      
+    def execute
+      inputs = YAML.load_file(@input_file)
+      inputs = hash_inputs_to_list(inputs) if inputs.is_a?(Hash)
+     
+      test_set = Pairwise.test_set(inputs)
+      display(test_set, input_names(inputs))
+    end
+
+    private
+    def hash_inputs_to_list(inputs_hash)
+      inputs_hash.map {|key, value| {key => value}}
+    end
+    
+    def input_names(inputs)
+      inputs.map{|input| input.keys}.flatten
+    end
         
-      def display(test_data, inputs)
-        print "|"
-        inputs.each do |key|
-          print key + "|"
-        end
-        puts
+    def display(test_data, inputs)
+      print "|"
+      inputs.each do |key|
+        print key + "|"
+      end
+      puts
 
-        test_data.each do |data|
-          print "|"
-          data.each {|datum| print datum + "|"}
-          puts
-        end
+      test_data.each do |data|
+        print "|"
+        data.each {|datum| print datum + "|"}
+        puts
       end
     end
   end
