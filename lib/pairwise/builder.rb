214870070f1275b41d0630687f3353b1a613c2f5
Joseph Wilk <joe@josephwilk.net>
2010-01-07 21:31:47 +0000

    Refactor: Extract method

--- lib/pairwise/builder.rb
+++ lib/pairwise/builder.rb
@@ -29,31 +29,32 @@ module Pairwise
     end
 
     def horizontal_growth(input_combinations, input_values_for_growth, previously_grown_input_values)
-      pairs = generate_pairs_between(input_values_for_growth, previously_grown_input_values, previously_grown_input_values.size)
+      uncovered_pairs = generate_pairs_between(input_values_for_growth, previously_grown_input_values, previously_grown_input_values.size)
 
       if input_combinations.size <= input_values_for_growth.size
-        input_combinations = input_combinations.enum_for(:each_with_index).map do |input_combination, input_index|
-          extended_input_combination = input_combination + [input_values_for_growth[input_index]]
-          pairs = remove_pairs_covered_by(extended_input_combination, pairs)
-          extended_input_combination
-        end
+        input_combinations, uncovered_pairs = grow_input_collections_and_remove_covered_pairs(input_combinations, input_values_for_growth, uncovered_pairs)
       else
-        input_combinations[0...input_values_for_growth.size] = input_combinations[0...input_values_for_growth.size].enum_for(:each_with_index).map do |input_combination, input_index|
-          extended_input_combination = input_combination + [input_values_for_growth[input_index]]
-          pairs = remove_pairs_covered_by(extended_input_combination, pairs)
-          extended_input_combination
-        end
+        input_combinations[0...input_values_for_growth.size], uncovered_pairs = grow_input_collections_and_remove_covered_pairs(input_combinations[0...input_values_for_growth.size], input_values_for_growth, uncovered_pairs)
 
         input_combinations[input_values_for_growth.size..-1] = input_combinations[input_values_for_growth.size..-1].map do |input_combination|
-          extended_input_combination = input_combination_that_covers_most_pairs(input_combination, input_values_for_growth, pairs)
-          pairs = remove_pairs_covered_by(extended_input_combination, pairs)
+          extended_input_combination = input_combination_that_covers_most_pairs(input_combination, input_values_for_growth, uncovered_pairs)
+          uncovered_pairs = remove_pairs_covered_by(extended_input_combination, uncovered_pairs)
           extended_input_combination
         end
       end
 
-      [input_combinations, pairs]
+      [input_combinations, uncovered_pairs]
     end
 
+    def grow_input_collections_and_remove_covered_pairs(input_combinations, input_values_for_growth, uncovered_pairs)
+      input_combinations = input_combinations.enum_for(:each_with_index).map do |input_combination, input_index|
+        extended_input_combination = input_combination + [input_values_for_growth[input_index]]
+        uncovered_pairs = remove_pairs_covered_by(extended_input_combination, uncovered_pairs)
+        extended_input_combination
+      end
+      [input_combinations, uncovered_pairs]
+    end
+        
     def vertical_growth(input_combinations, uncovered_pairs)
       new_input_combinations = []
 
