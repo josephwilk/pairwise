17b663d467b9407ead7fa223b063a92c6d72dc0c
Joseph Wilk <joe@josephwilk.net>
2012-08-21 13:57:00 +0100

    Refactor: Trying to extract out the boolean flag and push the decision logic into the method

--- lib/pairwise/ipo/horizontal.rb
+++ lib/pairwise/ipo/horizontal.rb
@@ -8,15 +8,14 @@ module Pairwise
           uncovered_pairs = PairCollection.new(input_values_for_growth, previously_grown_input_values, previously_grown_input_values.size)
 
           if input_combinations.size <= input_values_for_growth.size
-            input_combinations, uncovered_pairs = grow_input_combinations_and_remove_covered_pairs(input_combinations, input_values_for_growth, uncovered_pairs)
+            range_to_grow = 0..input_values_for_growth.size
+            input_combinations, uncovered_pairs = grow_input_combinations_and_remove_covered_pairs(input_combinations, range_to_grow, input_values_for_growth, uncovered_pairs)
           else
             number_of_list_of_values = input_values_for_growth.size
             
-            range_to_grow = 0...number_of_list_of_values 
-            input_combinations[range_to_grow], uncovered_pairs = grow_input_combinations_and_remove_covered_pairs(input_combinations[range_to_grow], input_values_for_growth, uncovered_pairs)
-
-            range_to_grow = number_of_list_of_values ..-1
-            input_combinations[range_to_grow], uncovered_pairs = grow_input_combinations_and_remove_covered_pairs(input_combinations[range_to_grow], input_values_for_growth, uncovered_pairs, true)
+            [0...number_of_list_of_values, number_of_list_of_values..-1].each do |range_to_grow|
+              input_combinations[range_to_grow], uncovered_pairs = grow_input_combinations_and_remove_covered_pairs(input_combinations, range_to_grow, input_values_for_growth, uncovered_pairs)
+            end
           end
 
           [input_combinations, uncovered_pairs]
@@ -24,9 +23,10 @@ module Pairwise
 
         private
       
-        def grow_input_combinations_and_remove_covered_pairs(input_combinations, input_values_for_growth, uncovered_pairs, flag=false)
-          input_combinations = input_combinations.enum_for(:each_with_index).map do |input_combination, input_index|
-            extended_input_combination = if flag
+        def grow_input_combinations_and_remove_covered_pairs(input_combinations, range_to_grow, input_values_for_growth, uncovered_pairs)
+          input_combinations = input_combinations[range_to_grow].enum_for(:each_with_index).map do |input_combination, input_index|
+
+            extended_input_combination = if range_to_grow.first != 0
               uncovered_pairs.input_combination_that_covers_most_pairs(input_combination, input_values_for_growth)
             else
               input_combination + [input_values_for_growth[input_index]]
