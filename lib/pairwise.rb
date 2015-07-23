4e99c8a4c3ec50e99ea32e92d6f9e297db8ae357
Sergey Morozov <sergey@slack-corp.com>
2015-07-22 18:12:39 -0700

    don't use sych for ruby 2.2.0 and above

--- lib/pairwise.rb
+++ lib/pairwise.rb
@@ -12,14 +12,14 @@ require 'pairwise/input_file'
 require 'pairwise/cli'
 
 require 'yaml'
-if RUBY_VERSION != '1.8.7'
+if RUBY_VERSION != '1.8.7' && RUBY_VERSION < '2.2.0'
   YAML::ENGINE.yamler = 'syck' 
 end
 
 module Pairwise
   class InvalidInputData < Exception; end
 
-  VERSION = '0.2.1'
+  VERSION = '0.2.2'
 
   class << self
     def combinations(*inputs)
